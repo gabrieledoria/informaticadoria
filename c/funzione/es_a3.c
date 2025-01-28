3a56328566cafeb8b8112b92636feb6ae823d965
gabrieledoria <geppetto875@gmail.com>
2025-01-28 10:08:13 +0000

    update

--- c/funzione/es_a3.c
+++ c/funzione/es_a3.c
@@ -1,1 +1,29 @@
-#include
+#include <stdio.h>
+
+int somma_divisori(int x)
+
+int main()
+
+{
+    int num1,totale;
+    prinf("\n\tInserisci un numero:  ");
+    scanf("%d", &num1);
+    totale= somma_divisori (num1);
+
+}
+
+int somma_divisori (int x);
+{
+    int somma= 0;
+    printf("\n\tDivisori di %d : ", x);
+    for( int cont = 1; cont <= x; cont++)
+    {
+        if (x % cont == 0)
+        {
+            printf("%d", cont);
+            somma += cont;
+        }
+    }
+    printf ("\n\tLa somma dei divisori di %d e : %d\n", x, somma);
+    return somma 
+}
