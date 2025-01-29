24c66df7e4835e3521395bbc14cd80112e8eac81
gabrieledoria <geppetto875@gmail.com>
2025-01-29 10:47:42 +0000

    update

--- c/E-Funzioni/ES01_concetti_base/es_a1_rettangolo.c
+++ c/E-Funzioni/ES01_concetti_base/es_a1_rettangolo.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-float area_rettangolo (float base , float altezza);
+float area_rettangolo(float base, float altezza);
 
-int main(){
-float base, altezza, area;
-printf("inserisci la base del rettagolo : ");
-scanf("%f", &base);
-
-printf("inserisci l'altezza del rettamgolo: ");
-scanf("%f", &altezza);
-
-area= area_rettangolo (base, altezza);
-
-printf("l'area del rettangolo è : %.2f\n", area);
-
-return 0; 
-}
-float area_rettangolo (float base , float altezza)
+int main()
 {
-    return base*altezza;
+    float base, altezza, area;
+    printf("inserisci la base del rettagolo : ");
+    scanf("%f", &base);
+
+    printf("inserisci l'altezza del rettamgolo: ");
+    scanf("%f", &altezza);
+
+    area = area_rettangolo(base, altezza);
+    printf("l'area del rettangolo è : %.2f\n", area);
+
+    return 0;
+}
+
+float area_rettangolo(float base, float altezza)
+{
+    return base * altezza;
 }
