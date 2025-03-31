9561140304289832cec70cd35b9d28653cc41f14
gabrieledoria <geppetto875@gmail.com>
2025-03-31 09:37:01 +0000

    update

---
+++ INFO/F-Strutture_dati/ES01_array_monodimensionali/es_a3.c
@@ -0,0 +1,10 @@
+*//
+/* Ricerca di un Elemento
+Dichiarare e inizializzare un array di 15 numeri con numeri random tra 0 e 20
+Si chieda all'utente un numero da cercare
+Si verifichi se il numero è presente nell'array
+Si stampi la posizione del numero se presente, altrimenti un messaggio appropriato
+
+doria  
+*/
+#include <stdio.h>
