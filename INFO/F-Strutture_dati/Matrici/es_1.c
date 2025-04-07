4074672c9d03a26ed23711e649c3fa138fe23e65
gabrieledoria <geppetto875@gmail.com>
2025-04-07 09:07:42 +0000

    up

--- INFO/F-Strutture_dati/Matrici/es_1.c
+++ INFO/F-Strutture_dati/Matrici/es_1.c
@@ -3,20 +3,38 @@
 
 void carica_matrice(char matrice[][RC]);
 void stampa_matrice(char matrice[][RC]);
+
 int main()
 {
     char matrice[RC][RC];
-    carica_matrice(matrice);
-    stampa_matrice(matrice);
+    carica_matrice(matrice);  
+    stampa_matrice(matrice);  
     return 0;
 }
+
 void carica_matrice(char matrice[][RC])
 {
+    
+    printf("Inserisci i valori per la matrice (5x5):\n");
     for (int i = 0; i < RC; i++)
     {
         for (int j = 0; j < RC; j++)
         {
-            printf("%c\t", matrice[i][j]);
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf(" %c", &matrice[i][j]);
+        }
+    }
+}
+
+void stampa_matrice(char matrice[][RC])
+{
+    // Print the matrix
+    printf("Matrice inserita:\n");
+    for (int i = 0; i < RC; i++)
+    {
+        for (int j = 0; j < RC; j++)
+        {
+            printf("%c\t", matrice[i][j]);  
         }
         printf("\n");
     }
