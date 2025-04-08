02830debe5a4c46251201e8854a6d691bfd1772b
gabrieledoria <geppetto875@gmail.com>
2025-04-08 07:32:52 +0000

    update

--- TPSIT/es_triangolo_d1.sh
+++ TPSIT/es_triangolo_d1.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Controlla che l'argomento sia un numero positivo
-if ! [[ "$1" =~ ^[0-9]+$ ]] || [ "$1" -le 0 ]; then
-    echo "Errore: inserisci un num positivo come argomento."
-    exit 1
-fi
-
-# Numero  righe del triangolo
+# # Salva il numero di righe del triangolo nell variabile 'n'
 n=$1
+
+# Inizializza il contatore per il ciclo che stampa le righe
 cont=$n
 
-# Ciclo per stampare il triangolo rovesciato
+# Ciclo per stampare un triangolo rovesciato
+# Ogni ciclo riduce il numero di "*" stampati
 while [ $cont -ge 1 ]; do
     j=1
     
+    # Stampa '*' per ogni riga, in base al numero di '*'
     while [ $j -le $cont ]; do
-        echo -n "*"
-        ((j++))
+        echo -n "*"  # Aggiunge un '*' senza andare a capo
+        ((j++))       # Incrementa j per continuare a stampare
     done
     
+    # Aggiungi un a capo per passare alla riga successiva
     echo
+    
+    # Riduci il numero di '*' per la prossima riga
     ((cont--))
 done
